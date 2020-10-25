a8380a1d62a68458e16fc241136e4d4cd5b07273
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-10-25 19:20:18 +0300

    Add all types

--- Faker/FakerLibrary/Generator.cs
+++ Faker/FakerLibrary/Generator.cs
@@ -16,7 +16,72 @@ namespace FakerLibrary
             return 0;
         }
 
-        public static byte GenerateByte()
+        public static byte GenerateChar()
+        {
+            return 0;
+        }
+
+        public static byte GenerateSByte()
+        {
+            return 0;
+        }
+
+        public static byte GenerateInt16()
+        {
+            return 0;
+        }
+
+        public static byte GenerateUInt16()
+        {
+            return 0;
+        }
+
+        public static byte GenerateInt32()
+        {
+            return 0;
+        }
+
+        public static byte GenerateUInt32()
+        {
+            return 0;
+        }
+
+        public static byte GenerateInt64()
+        {
+            return 0;
+        }
+
+        public static byte GenerateUInt64()
+        {
+            return 0;
+        }
+
+        public static byte GenerateSingle()
+        {
+            return 0;
+        }
+
+        public static byte GenerateDouble()
+        {
+            return 0;
+        }
+
+        public static byte GenerateDecimal()
+        {
+            return 0;
+        }
+
+        public static byte GenerateDateTime()
+        {
+            return 0;
+        }
+
+        public static byte GenerateString()
+        {
+            return 0;
+        }
+
+        public static byte GenerateList()
         {
             return 0;
         }
