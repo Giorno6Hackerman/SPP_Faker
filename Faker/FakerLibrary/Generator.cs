37e53220f0afa55607d10a5e20babe57dca29725
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-10-25 19:13:13 +0300

    Change generator

---
+++ Faker/FakerLibrary/Generator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakerLibrary
+{
+    public static class Generator
+    {
+        public static bool GenerateBoolean()
+        {
+            return true;
+        }
+
+        public static byte GenerateByte()
+        {
+            return 0;
+        }
+
+        public static byte GenerateByte()
+        {
+            return 0;
+        }
+    }
+}
