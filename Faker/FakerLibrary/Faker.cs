098c1ec4948b44f04f626bb47f8bdef3dee06443
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-10-26 14:51:54 +0300

    Fixed list

--- Faker/FakerLibrary/Faker.cs
+++ Faker/FakerLibrary/Faker.cs
@@ -4,42 +4,17 @@ namespace FakerLibrary
 {
     public class Faker
     {
-        public delegate T Generate<T>();
+        public delegate object Generate();
+
 
         public Faker()
         { 
         
         }
 
-        public static Generate<T> GetGenerator<T>()
+        public static Generate GetGenerator(Type type)
         {
-            string typeName = typeof(T).Name;
-            if (typeName.StartsWith("List<"))
-            {
-                string subType = typeName.Substring(5, typeName.IndexOf(">") - 5);
-            }
-            switch (typeName)
-            {
-                case "Boolean":
-                    Generate<bool> gen = ListGenerator.GenerateBoolean;
-                    break;
-                case "Byte":
-                case "SByte":
-                case "Char":
-                case "Int16":
-                case "UInt16":
-                case "Int32":
-                case "UInt32":
-                case "Int64":
-                case "UInt64":
-                case "Single":
-                case "Double":
-                case "Decimal":
-                case "DateTime":
-                case "String":
-                default:
-                    break;
-            }
+            
             return null;
         }
     }
--- Faker/FakerLibrary/ListGenerator.cs
+++ Faker/FakerLibrary/ListGenerator.cs
@@ -6,17 +6,14 @@ namespace FakerLibrary
 {
     public static class ListGenerator
     {
-        private static Random _rand = new Random();
-
-        
         // определять тип элементов и вызывать сответствующий генератор
-        public static List<T> GenerateList<T>()
+        public static List<T> GenerateList<T>(Random random)
         {
-            int size = _rand.Next(Byte.MaxValue);
+            int size = random.Next(Byte.MaxValue);
             List<T> list = new List<T>(size);
             for (int i = 0; i < size; i++)
             {
-                list.Add(Faker.GetGenerator<T>().Invoke());
+                list.Add((T)Faker.GetGenerator(typeof(T)).Invoke());
             }
             return list;
         }
