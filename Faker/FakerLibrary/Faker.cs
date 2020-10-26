fc8883373f7be21109ed5b748ac52c147b07138e
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-10-26 12:18:41 +0300

    Add files via upload

--- Faker/FakerLibrary/Faker.cs
+++ Faker/FakerLibrary/Faker.cs
@@ -13,6 +13,33 @@ namespace FakerLibrary
 
         public static Generate<T> GetGenerator<T>()
         {
+            string typeName = typeof(T).Name;
+            if (typeName.StartsWith("List<"))
+            {
+                string subType = typeName.Substring(5, typeName.IndexOf(">") - 5);
+            }
+            switch (typeName)
+            {
+                case "Boolean":
+                    Generate<bool> gen = Generator.GenerateBoolean;
+                    return gen;
+                case "Byte":
+                case "SByte":
+                case "Char":
+                case "Int16":
+                case "UInt16":
+                case "Int32":
+                case "UInt32":
+                case "Int64":
+                case "UInt64":
+                case "Single":
+                case "Double":
+                case "Decimal":
+                case "DateTime":
+                case "String":
+                default:
+                    break;
+            }
             return null;
         }
     }
